94b01ded636c9b3b2c6e529d0d96b54806ab47af
hilalbeyzakgl@gmail.com <hilalbeyzakgl@gmail.com>
2024-04-02 14:58:09 +0300

    first commit

--- Soru1/Program.cs
+++ Soru1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        var codes = AddCodeList();
+        Console.WriteLine(string.Join("\t", codes));
+    }
+    private static List<string> AddCodeList()
+    {
+        List<string> codes = new List<string>();
+        string generated_code = "";
+        bool control = false;
+        for (int i = 0; i < 1000; i++)
+        {
+            generated_code = CreateCode();
+            control = CodeUniqueControl(codes, generated_code);
+            if (!control)
+            {
+                codes.Add(generated_code);
+            }
+            else
+            {
+                --i;
+            }
+
+        }
+        return codes;
+
+    }
+
+    private static string CreateCode()
+    {
+        int length = 8;
+        string validChars = "ACDEFGHKLMNPRTXYZ234579";
+        Random random = new Random();
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            chars[i] = validChars[random.Next(0, validChars.Length)];
+        }
+        return new string(chars);
+
+    }
+
+    private static bool CodeUniqueControl(List<string> codes, string code)
+    {
+        return codes.Contains(code);
+
+    }
+}
